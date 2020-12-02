50bce980399bd87bdc4b90c6ed6e11d2d6ea991b
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-12-02 15:55:05 +0200

    commit first

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var inp1;
-var inp2;
-var inp3;
+var inp = document.querySelectorAll("input");
+var text = document.getElementById("text");
 
 setInterval(function() {
-    inp1 = document.getElementById("inp1").value;
-    inp2 = document.getElementById("inp2").value;
-    inp3 = document.getElementById("inp3").value;
-
-    document.getElementById("text").value += `${inp1}, ${inp2}, ${inp3}`;
+    for (let i = 0; i < inp.length; i++) {
+        if (inp[i].value !== "") {
+            text.value  += `${inp[i].value},`;
+        }
+    }
+    
 }, 1000);
